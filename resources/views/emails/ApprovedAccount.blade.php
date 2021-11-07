0b11b1b1eed5663803d64772f6966d484e49f428
Yvan Sabay <sabayyvan2018@gmail.com>
2021-11-07 19:35:39 +0800

    added queues

--- resources/views/emails/ApprovedAccount.blade.php
+++ resources/views/emails/ApprovedAccount.blade.php
@@ -1,7 +1,7 @@
 @component('mail::message')
 Greetings, {{$last_name}} your account was approved by the system administrator.
 You can now sign-in to your account.
-
+<br/>
 User Information <br/>
 First Name: {{$first_name}} <br/>
 Middle Name: {{$middle_name}} <br/>
